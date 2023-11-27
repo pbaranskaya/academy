60783bfa95b68a5c49c767ecd60c7ccbe234fe13
Andrey <andrey-tgsv@mail.ru>
2023-11-27 21:34:52 +0300

    lesson 8

---
+++ src/by/academy/lesson8/Product.java
@@ -0,0 +1,62 @@
+package by.academy.lesson8;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Product {
+
+    String name;
+    double price;
+    double quantity;
+
+//    public Product() {
+//        super();
+//    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(product.price, price) == 0 && Double.compare(product.quantity, quantity) == 0 && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+}
