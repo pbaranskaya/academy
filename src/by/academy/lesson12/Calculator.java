a591c329078ad91d541567ab192619b6a73d08ae
Andrey <andrey-tgsv@mail.ru>
2023-12-27 21:37:27 +0300

    lesson 12

---
+++ src/by/academy/lesson12/Calculator.java
@@ -0,0 +1,13 @@
+package by.academy.lesson12;
+
+public class Calculator {
+
+    public static <X extends Number, Y extends Number> double sum (X x, Y y) {
+        return x.doubleValue() + y.doubleValue();
+    }
+
+    public static void main(String[] args) {
+        double result = Calculator.sum(1,4.5);
+        System.out.println(result);
+    }
+}
---
+++ src/by/academy/lesson12/ProductBox.java
@@ -0,0 +1,21 @@
+package by.academy.lesson12;
+
+import by.academy.DealHW.Product;
+
+public class ProductBox {
+
+    private Product product;
+
+
+    public ProductBox(Product product){
+        this.product = product;
+    }
+
+    public void setProduct(){
+        this.product = product;
+    }
+
+    public Product getProduct(){
+        return product;
+    }
+}
