57ed25d1eb2350ba066d7f6c69812a2a6609b215
Andrey <andrey-tgsv@mail.ru>
2024-02-19 21:30:08 +0300

    lesson 24

---
+++ src/by/academy/lesson24/Address.java
@@ -0,0 +1,40 @@
+package by.academy.lesson24;
+
+public class Address{
+    private int doorNo;
+    private String street;
+    private String city;
+    private String zip;
+
+    public int getDoorNo() {
+        return doorNo;
+    }
+
+    public void setDoorNo(int doorNo) {
+        this.doorNo = doorNo;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+}
